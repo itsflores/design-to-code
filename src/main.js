166c61461878b55b89e8e13957e376dfd7c24e5b
Omar <omar.garciaf247@gmail.com>
2019-11-23 11:20:54 -0500

    got colors

--- src/main.js
+++ src/main.js
@@ -1,13 +1,20 @@
 const FIGMA_API_URL = 'https://api.figma.com/v1';
 const ACCESS_TOKEN = '26651-625efab7-5897-4196-a328-b1c88876d26d';
-const request = require('request');
 const fetch = require('node-fetch');
+
+const designSystem = {};
 
 const options = {
     headers: {
         'X-Figma-Token': `${ACCESS_TOKEN}`
     }
-};
+}
+
+const generateColors = (colorFrames) => {
+    colorFrames.forEach((frame) => {
+        designSystem[frame.name] = `color: ${frame.children[0].characters}`;
+    });
+}
 
 const figmaAPIRequest = async() => {
     const res = await fetch(`${FIGMA_API_URL}/files/jZE8coiDiHCIRoF0mndbkp`, options, async(error, response, body) => {
@@ -19,13 +26,19 @@ const figmaAPIRequest = async() => {
     return Promise.resolve(figmaRes);
 }
 
-const whatever = (figma) => {
-    console.log(figma);
+const generateClasses = (designObj) => {
+    Object.keys(designObj).forEach((frame) => {
+        if (designObj[frame].type === 'FRAME') {
+            // ?Gets colors from 
+            if (designObj[frame].name === 'colors') {
+                generateColors(designObj[frame].children);
+            }
+        }
+    })
 }
 
 (async() => {
+    let classes = [];
     const figmaObj = await figmaAPIRequest();
-    // console.log('connection : %j', figmaObj);
-    console.log(figmaObj.document.children);
-    whatever(figmaObj.document.children);
+    classes = generateClasses(figmaObj.document.children[0].children[1].children);
 })();

55e809432d1d71ac37e7f8bde274e9da9a575936
Omar <omar.garciaf247@gmail.com>
2019-11-23 01:44:08 -0500

    setting up API

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const FIGMA_API_URL = 'https://api.figma.com/v1';
+const ACCESS_TOKEN = '26651-625efab7-5897-4196-a328-b1c88876d26d';
+const request = require('request');
+const fetch = require('node-fetch');
+
+const options = {
+  headers: {
+    'X-Figma-Token': `${ACCESS_TOKEN}`
+  }
+};
+
+const figmaAPIRequest = async() => {
+  const res = await fetch( `${FIGMA_API_URL}/files/jZE8coiDiHCIRoF0mndbkp`, options, async(error, response, body) => {
+    const object = JSON.parse(body);
+    return Promise.resolve(object);
+  });
+ 
+  const figmaRes = await res.json();
+  return Promise.resolve(figmaRes);
+}
+
+(async() => {
+  const figmaObj = await figmaAPIRequest();
+  // console.log('connection : %j', figmaObj);
+  console.log(figmaObj.document.children);
+})();
